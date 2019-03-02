0343979221ac04f2e09f371e84ce0a52fec6ca55
Nicolas <nicolas_tiseira@hotmail.com>
2019-03-02 11:19:58 -0300

    initial commit

---
+++ src/GeneralParser/GeneralParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GeneralParser
+{
+    public static class GeneralParser
+    {
+        public static DateTime ParseDateInvariant(this string dateToParse)
+        {
+            DateTime dt;
+
+            try
+            {
+                dt = DateTime.ParseExact(dateToParse, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            }
+            catch (Exception)
+            {
+
+                dt = DateTime.MinValue;
+            }
+
+            return dt;
+        }
+    }
+}
